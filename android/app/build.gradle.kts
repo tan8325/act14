88c7507d8b96770dd3ff34c33101b9783102f05f
tan8325 <tbach4@student.gsu.edu>
2025-04-09 14:00:50 -0400

    Added types of notification, notificaiton history, and notification icons and action buttons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
 android {
     namespace = "com.example.act14"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
+    ndkVersion = "27.0.12077973"
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
